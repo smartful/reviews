6f8ea54f3671a452efb22283f6170699b9d8e25a
smartful <remi_rodrigues@hotmail.com>
2021-03-27 00:13:35 +0100

    add mechanisms of arrows and random button

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types'
 import styles from '../styles/Home.module.css'
 import Arrows from './Arrows'
 
-function Card({ imgsrc, name, job, description }) {
+function Card({ imgsrc, name, job, description, incrementIndex, decrementIndex, randomIndex }) {
   return (
     <div className={styles.card}>
       <img src={imgsrc} alt={name} className={styles.profil}/>
       <div className={styles.name}>{name}</div>
       <div className={styles.job}>{job}</div>
       <div className={styles.description}>{description}</div>
-      <Arrows />
-      <button className={styles.random}>Aléatoire</button>
+      <Arrows
+        incrementIndex={incrementIndex}
+        decrementIndex={decrementIndex}
+      />
+      <button className={styles.random} onClick={() => randomIndex()}>
+        Aléatoire
+      </button>
     </div>
   );
 }
@@ -20,7 +25,10 @@ Card.propTypes = {
   imgsrc: PropTypes.string,
   name: PropTypes.string,
   job: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
+  incrementIndex: PropTypes.func,
+  decrementIndex: PropTypes.func,
+  randomIndex: PropTypes.func
 }
 
 export default Card;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { employees } from '../data/employees'
 import Card from '../components/Card'
 
 export default function Home() {
+  const [employeeIndex, setEmployeeIndex] = useState(0)
+  const [employee, setEmployee] = useState(employees[0])
+
+  const incrementIndex = () => {
+    let newIndex = employeeIndex + 1;
+    if (newIndex < employees.length) {
+      setEmployeeIndex(newIndex)
+    } else {
+      setEmployeeIndex(0)
+    }
+  }
+
+  const decrementIndex = () => {
+    let newIndex = employeeIndex - 1;
+    if (newIndex >= 0) {
+      setEmployeeIndex(newIndex)
+    } else {
+      setEmployeeIndex(employees.length - 1)
+    }
+  }
+
+  const randomIndex = () => {
+    const newIndex = Math.floor(Math.random() * employees.length);
+    setEmployeeIndex(newIndex)
+  }
+
+  useEffect(() => {
+    setEmployee(employees[employeeIndex])
+  }, [employeeIndex])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,15 +45,18 @@ export default function Home() {
       <h1 className={styles.title}>Reviews</h1>
       <div className={styles.underline} />
 
-      {employees.map((employee) => (
+      {employee &&
         <Card
           key={employee.id}
           imgsrc={employee.imgsrc}
           name={employee.name}
           job={employee.job}
           description={employee.description}
+          incrementIndex={incrementIndex}
+          decrementIndex={decrementIndex}
+          randomIndex={randomIndex}
         />
-      ))}
+      }
     </div>
   )
 }
